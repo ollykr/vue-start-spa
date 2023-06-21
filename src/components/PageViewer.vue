7436c05a28143fb7769ce8b3afb8a899d2e83ffa
ollykr <okremianski@gmail.com>
2023-06-21 17:56:26 -0400

    Update page-viewwer with props

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,12 +1,28 @@
 <template>
     <div class="container">
-        <h1>{{ page.pageTitle }}</h1>
+        <h1 class="emphasize">{{ page.pageTitle }}</h1>
         <p>{{ page.content }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ["page"]
+    props: {
+        page: {
+            // since the type is an Object, we have to write a function
+            type: Object,
+            default(rawProps) {
+                return {
+                    pageTitle: '',
+                    content: '',
+                };
+            }
+        }
+    }
  			}
 </script>
+<style scoped>
+.emphasize {
+    color: blue;
+}
+</style>
