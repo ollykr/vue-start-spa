19a270b27d4c9eb28522e8a7c896aa91086b155d
ollykr <okremianski@gmail.com>
2023-07-07 18:17:56 -0400

    Use v-model to bind data in a form

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -5,10 +5,10 @@
             <label for="" class="form-label">
                 Page Title
             </label>
-            <!-- @input registers the whatever value added inside pageTitle form field in a browser, @input="(e) => pageTitle = e.target" it is two-way binding-->
+            <!-- @input registers the whatever value added inside pageTitle form field in a browser, @input="(e) => pageTitle = e.target" it is two-way binding, v-model does the same but it is simpler-->
+            <!-- v-model binds a data property to that form control -->
             <input type="text" class="form-control"
-            :value="pageTitle"
-            @input="(e) => pageTitle = e.target.value"
+            v-model="pageTitle"
             >
         </div>
         <div class="mb-3"><label for="" class="form-label">
@@ -18,13 +18,15 @@
             id=""
             cols="30"
             rows="10"
-            class="form-control">
+            class="form-control"
+            v-model="content"
+            >
         </textarea>
             </div>
         <div class="mb-3">
             <button
              class="btn btn-primary"
-             @click.prevent="pageCreated({pageTitle})"
+             @click.prevent="pageCreated({pageTitle, content})"
              >Create Page</button>
             </div>
     </form>
@@ -35,11 +37,12 @@
 <script>
 // Simple prop definition
 export default {
-    props: ['pageCreated'],
+    props: ['pageCreated', 'content'],
     data() {
         return {
-    pageTitle: ''
-}
+            pageTitle: '',
+            content: ''
+        }
 
     }
 }
