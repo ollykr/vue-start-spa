1a27a27bfaaecdbb147510d2e870754b7ca30e81
ollykr <okremianski@gmail.com>
2023-07-11 12:38:04 -0400

    Add error validation to the form

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,7 +1,10 @@
 <template>
 <div class="container mb-3">
+    <!-- v-model creates two way binding -->
     <form action="">
-        <div class="mb-3">
+        <div class="row">
+        <div class="col-md-8">
+            <div class="mb-3">
             <label for="" class="form-label">
                 Page Title
             </label>
@@ -10,38 +13,98 @@
             <input type="text" class="form-control"
             v-model="pageTitle"
             >
-        </div>
-        <div class="mb-3"><label for="" class="form-label">
+            </div>
+            <div class="mb-3"><label for="" class="form-label">
                     Content
                 </label>
-            <textarea name=""
-            id=""
-            cols="30"
-            rows="10"
-            class="form-control"
-            v-model="content"
-            >
-        </textarea>
+                <textarea name=""
+                id=""
+                cols="30"
+                rows="10"
+                class="form-control"
+                v-model="content"
+                >
+                </textarea>
             </div>
+        </div>
+        <div class="col">
+            <div class="mb-3">
+                <label for="" class="form-label">
+                Link Text
+                </label>
+                <input type="text" class="form-control"
+                v-model="linkText"
+                >
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Link URL
+                </label>
+                <input type="text" class="form-control"
+                v-model="linkUrl"
+                >
+            </div>
+            <div class="row mb-3">
+                <div class="form-check">
+                    <input id="gridCheck1" class="form-check-input" type="checkbox">
+                    <label for="gridCheck1" class="form-check-label">
+                        Published
+                    </label>
+                </div>
+            </div>
+        </div>
         <div class="mb-3">
+            <!-- submitForm method -->
+            <!-- When disabled is true, then the button is disabled -->
             <button
              class="btn btn-primary"
-             @click.prevent="pageCreated({pageTitle, content})"
+             :disabled="isFormInvalid"
+             @click.prevent="submitForm"
              >Create Page</button>
-            </div>
+        </div>
+    </div>
     </form>
 </div>
 
 </template>
 
 <script>
-// Simple prop definition
+// Simple prop definitions
 export default {
-    props: ['pageCreated', 'content'],
+    props: ['pageCreated'],
+    // for visual cues error validation
+    computed: {
+        isFormInvalid() {
+            // return true if none of the fields are filled
+            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+        }
+    },
     data() {
         return {
             pageTitle: '',
-            content: ''
+            content: '',
+            linkText: '',
+            linkUrl: ''
+        }
+    },
+    methods: {
+        // we don't need to add any external data such as an event object (e) e.g submitForm(e) because all the data we need is in data() function, e.g pageTitle, content, etc.
+        submitForm() {
+            // make sure we have some values
+            // if one of these values is missing , we can't create the object
+            if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+                alert('Please fill the form.');
+                return;
+            }
+// if do have all the data, then we pass in the object that has pageTitle, content, etc
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                content: this.content,
+                link: {
+                    text: this.linkText,
+                    url: this.linkUrl
+                }
+            })
         }
 
     }
