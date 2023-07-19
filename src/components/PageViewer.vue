ae56d3f835e77a98778d1f608b95910688baa7b2
ollykr <okremianski@gmail.com>
2023-07-19 17:30:14 -0400

    Set up params

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,24 +1,19 @@
 <template>
     <div class="container">
         <h1 class="emphasize">Page Title</h1>
-        <p>content</p>
+        <p>{{$route.params.index}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    props: {
-        page: {
-            // since the type is an Object, we have to write a function
-            type: Object,
-            default(rawProps) {
-                return {
-                    pageTitle: '',
-                    content: '',
-                };
-            }
-        }
-    }
+    created() {
+        // this.$route is the router currently been handled by this page viewer component
+        // params is the object that contains all the router's params inside router's path
+        // right now we have just one param - an index
+        // use console.log to see params in a browser console
+        // console.log(this.$route.params);
+}
  			}
 </script>
 <style scoped>
--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,11 @@ import CreatePage from "./components/CreatePage";
 // Create our router
 // Since we are using router , we can't really use props , all the date would be coming via URL, not from a parent component
 // Home route is our PageViewer component
-
+// ":/index?" is a unique router param (optinal) for home/index page url
 const router = createRouter({
 	history: createWebHashHistory(),
 	routes: [
-		{ path: "/", component: PageViewer },
+		{ path: "/:index?", component: PageViewer },
 		{ path: "/create", component: CreatePage },
 	],
 });
