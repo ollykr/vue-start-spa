3b2ff6c3986d163705ec0c5c69407be90e1fbf65
ollykr <okremianski@gmail.com>
2023-08-04 14:23:03 -0400

    A counter via composition api - second way

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,5 +1,5 @@
 <template>
-    <p>{{counter}}</p>
+    <p>{{data.counter}}</p>
     <!-- "click" is a method here -->
     <button @click.prevent="click">Click</button>
 </template>
@@ -8,23 +8,16 @@
 
 <!-- Compositions API - more convenient especially for larger projects -->
 <!-- // loads before components -->
-
+<!-- Second way -->
 <script setup>
 import { ref, reactive } from "vue";
-
-let counter = ref(0); // {value: 0}
+// anything considered an object you can use reactive of
+const data = reactive({ counter: 0 });
 
 function click() {
-    counter.value++;
+    // counter.value++;
+    data.counter++;
 }
-
-// without ref, the below code works (see via console.log) but it doesn't get reflected on a page , i.e click the button, nothing happens - it uses simple vars, it is NOT reactive. Behind the scenes Vue doesn't know "counter" value is being changed
-// let counter = 0;
-
-// function click() {
-//     counter++;
-//     console.log(counter);
-// }
 </script>
 
 <!-- <script>
